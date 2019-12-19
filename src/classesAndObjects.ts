c898f76ac3f1ecd6ed9b8056af704b01c6035c5c
Sam Redmond <sredmond@techusolutions.com>
2019-12-19 12:22:29 -0600

    Demonstrating a basic class

---
+++ src/classesAndObjects.ts
@@ -0,0 +1,31 @@
+export class Person {
+  name: string;
+  age: number;
+
+  private weightInLB: number;
+
+  constructor(name: string, age: number, weight?: number) {
+    this.name = name;
+    this.age = age;
+
+    if (weight) {
+      this.weightInLB = weight;
+    }
+  }
+
+  set weight(weight: number) {
+    this.weightInLB = weight;
+  }
+
+  get weight() {
+    return this.weightInLB;
+  }
+
+  convertWeightToKG() {
+    if (this.weight) {
+      return Math.round(this.weight * 2.2);
+    }
+
+    return null;
+  }
+}
