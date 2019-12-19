8a3f65e8fc25951248af7ac4a4afe1ab5a76f429
Sam Redmond <sredmond@techusolutions.com>
2019-12-19 12:12:16 -0600

    Export all functions from index

--- src/myFunctions/index.ts
+++ src/myFunctions/index.ts
@@ -1,4 +1,9 @@
-import * as myFunctions from "./myfunctions";
+export * from './myfunctions';
 
+
+import * as myFunctions from "./myfunctions";
+import { subtract } from "./myfunctions";
+
+const subtracted = subtract(2, 1);
 const newNumber = myFunctions.add(1, 2);
 const greeting = myFunctions.sayHello("Sam");
