5acaa5c35fbc89a80087a514fcfa6e0780bec10d
Sam Redmond <sredmond@techusolutions.com>
2019-12-19 13:33:19 -0600

    Adding our first route

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import * as express from "express";
 import { createServer } from "http";
+import * as bodyParser from "body-parser";
+import * as cors from "cors";
+import * as morgan from "morgan";
 
 const app = express();
 const server = createServer(app);
+
+app.use(cors({ origin: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan("dev"));
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  return res.json({ hello: "world" });
+});
 
 server.listen(8080, () => {
   console.log("User management API running");
