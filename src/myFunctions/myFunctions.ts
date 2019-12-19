12f2e5b0a2cb0e69c4076e486918df74290e71ca
Sam Redmond <sredmond@techusolutions.com>
2019-12-19 12:04:00 -0600

    Creating functions

---
+++ src/myFunctions/myFunctions.ts
@@ -0,0 +1,11 @@
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+function subtract(num1: number, num2: number) {
+  return num1 - num2;
+}
+
+function sayHello(name: string) {
+  return "Hello " + name;
+}
