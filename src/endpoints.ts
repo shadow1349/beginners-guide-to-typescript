ade80be32f2817917123649672b10737480547b8
Sam Redmond <sredmond@techusolutions.com>
2019-12-19 15:34:44 -0600

    Adding endpoints

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -1,3 +1,26 @@
 import * as express from "express";
+import { users } from "./data";
 
-export const Router = express.Router();
+export const UserRouter = express.Router();
+
+UserRouter.get("/", (req: express.Request, res: express.Response) => {
+  return res.status(200).json({ users });
+});
+
+UserRouter.get("/:id", (req: express.Request, res: express.Response) => {
+  const id = Number.parseInt(req.params.id);
+
+  if (Number.isNaN(id)) {
+    return res.status(400).json({ error: "Id is not a number" });
+  }
+
+  const index = users.findIndex(x => x.id === id);
+
+  if (index >= 0) {
+    const user = users[index];
+
+    return res.status(200).json({ user });
+  }
+
+  return res.status(400).json({ error: "Something went wrong" });
+});
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { createServer } from "http";
 import * as bodyParser from "body-parser";
 import * as cors from "cors";
 import * as morgan from "morgan";
+import { UserRouter } from "./endpoints";
 
 const app = express();
 const server = createServer(app);
@@ -11,6 +12,8 @@ app.use(cors({ origin: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan("dev"));
+
+app.use("/users", UserRouter);
 
 server.listen(8080, () => {
   console.log("API running");
