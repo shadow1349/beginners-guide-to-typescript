0d376d676d665f271b97576a25175cb66bdce206
Sam Redmond <sredmond@techusolutions.com>
2019-12-19 12:28:02 -0600

    Removing multiple imports

--- src/myFunctions/index.ts
+++ src/myFunctions/index.ts
@@ -1,9 +1,2 @@
 export * from "./myfunctions";
 export * from "./otherFunctions";
-
-import * as myFunctions from "./myfunctions";
-import { subtract } from "./myfunctions";
-
-const subtracted = subtract(2, 1);
-const newNumber = myFunctions.add(1, 2);
-const greeting = myFunctions.sayHello("Sam");
--- src/objectLiterals.ts
+++ src/objectLiterals.ts
@@ -1,7 +1,7 @@
-const person = {
+export const person = {
   name: "John Doe",
   age: 123,
   getFirstName() {
-    return this.name.split(" ")[0];
+    return name.split(" ")[0];
   }
 };
