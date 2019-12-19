ade54580b606e6a83a42f287e51a5f5789c43bdb
Sam Redmond <sredmond@techusolutions.com>
2019-12-19 13:49:45 -0600

    Adding data

---
+++ src/data.ts
@@ -0,0 +1,12 @@
+export const users = [
+  { id: 1, name: "John", age: 21 },
+  { id: 2, name: "Jack", age: 20 },
+  { id: 3, name: "Jill", age: 25 },
+  { id: 4, name: "Sam", age: 30 },
+  { id: 5, name: "Bob", age: 55 },
+  { id: 6, name: "Jane", age: 18 },
+  { id: 7, name: "Sean", age: 56 },
+  { id: 8, name: "Jacob", age: 40 },
+  { id: 9, name: "Alice", age: 35 },
+  { id: 10, name: "Lane", age: 23 }
+];
