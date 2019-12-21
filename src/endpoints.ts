6a4f86fa3ca171f4cf98ec0704b5d5e91d13e7a6
Sam Redmond <sredmond@techusolutions.com>
2019-12-20 18:04:49 -0600

    Added endpoints for creating users and deleting users

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -1,7 +1,22 @@
 import * as express from "express";
 import { users } from "./data";
+import { IUser } from "./models";
 
 export const UserRouter = express.Router();
+
+UserRouter.put("/", (req: express.Request, res: express.Response) => {
+  if (!req.body || !req.body.user) {
+    return res.status(400).json({ error: "Please send a valid user document" });
+  }
+
+  const user: IUser = req.body.user;
+
+  user.id = users.length + 1;
+
+  users.push(user);
+
+  return res.status(200).json({ user });
+});
 
 UserRouter.get("/", (req: express.Request, res: express.Response) => {
   return res.status(200).json({ users });
@@ -16,11 +31,29 @@ UserRouter.get("/:id", (req: express.Request, res: express.Response) => {
 
   const index = users.findIndex(x => x.id === id);
 
-  if (index >= 0) {
-    const user = users[index];
-
-    return res.status(200).json({ user });
+  if (index === -1) {
+    return res.status(400).json({ error: `No user found with id ${id}` });
   }
 
-  return res.status(400).json({ error: `No user found with id ${id}` });
+  const user = users[index];
+
+  return res.status(200).json({ user });
+});
+
+UserRouter.delete("/:id", (req: express.Request, res: express.Response) => {
+  const id = Number.parseInt(req.params.id);
+
+  if (Number.isNaN(id)) {
+    return res.status(400).json({ error: "Id is not a number" });
+  }
+
+  const index = users.findIndex(x => x.id === id);
+
+  if (index === -1) {
+    return res.status(400).json({ error: "" });
+  }
+
+  users.splice(index, 1);
+
+  return res.status(200).json({ message: "User deleted", id });
 });
