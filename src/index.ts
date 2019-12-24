4dd23c21293cac10c3294c8f773c8458ae5cbb2e
Sam Redmond <sredmond@techusolutions.com>
2019-12-23 22:03:28 -0600

    Adding example usage

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,11 @@ import Database from "./database";
 (() => {
   const database = new Database();
 
-  const collection = database.createCollection("Users");
+  const collection = database.createCollection<{ name: string }>("Users");
+
+  const userDocuments = collection.getDocuments();
 
   collection.addDocument({ name: "John" });
+
+  console.log(userDocuments);
 })();
